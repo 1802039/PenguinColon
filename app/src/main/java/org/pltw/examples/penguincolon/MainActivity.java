c1f45e9c8cb2ad40ffc644111a463a53cd28c130
karls17 <karlschmitt190@gmail.com>
2018-02-26 14:17:45 -0600

    Idk what im doing anymore

--- app/src/main/java/org/pltw/examples/penguincolon/MainActivity.java
+++ app/src/main/java/org/pltw/examples/penguincolon/MainActivity.java
@@ -3,6 +3,8 @@ package org.pltw.examples.penguincolon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static org.pltw.examples.penguincolon.Penguin.getPingus;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,11 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Penguin minHunt;
-        Penguin pingus;
-
-        if (minHunt >= pingus) {
-        }
         }
 
 
--- app/src/main/java/org/pltw/examples/penguincolon/Penguin.java
+++ app/src/main/java/org/pltw/examples/penguincolon/Penguin.java
@@ -5,8 +5,23 @@ package org.pltw.examples.penguincolon;
  */
 
 public class Penguin {
-    public double breedRate = .25;
+    public double breedRate = .25; //Here for future purposes
     public static int minHunt = 2;
     public static int pingus=20;
+
+    public static int getMinHunt() {
+        return minHunt;
+    }
+
+    public static int getPingus() {
+        return pingus;
+    }
+
+    public static void setPingus(int pingus) {
+        Penguin.pingus = pingus;
+    }
+
+
+
 
 }
